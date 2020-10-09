0c9f82c5ca6a0f1d6cbc9360bc3c1712214f62ce
EOM Design Group <eommail@gmail.com>
2020-10-09 20:14:56 -0300

    mejor en el control de cargar de componente si ya existe cargado

--- Controller/Component/LoadSubComponentsComponent.php
+++ Controller/Component/LoadSubComponentsComponent.php
@@ -19,21 +19,25 @@ class LoadSubComponentsComponent extends Component
     public function Load($path = '', $pre_path = APP . 'Controller/Component/')
     {
         // Is Empty..?
-        if (empty($path)) {
+        if (empty($path) === true) {
             return false;
         }
 
-        // Get Name SubFolder
-        $path_folder = substr($path,0, strripos($path, '/')+1);
-
-        // Join Path Full SubFolder
-        $load_folder = $pre_path . $path_folder;
-
-        // New set path SubFolder load component
-        App::build(array('Controller/Component' => array($load_folder)));
-
         // Get name component
-        $component_name = substr($path, strripos($path, '/')+1);
+        $component_name = substr($path, strripos($path, '/') + 1);
+
+        // Was not loaded..?
+        if ($this->_Collection->loaded($component_name) === false) {
+
+            // Get Name SubFolder
+            $path_folder = substr($path, 0, strripos($path, '/') + 1);
+
+            // Join Path Full SubFolder
+            $load_folder = $pre_path . $path_folder;
+
+            // New set path SubFolder load component
+            App::build(array('Controller/Component' => array($load_folder)));
+        }
 
         // Component Load
         return $this->_Collection->load($component_name);
